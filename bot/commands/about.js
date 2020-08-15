1f9d5fba4841ebae7485c1e5f3cd10d55d86e6df
Brendan Root <12brendan3@users.noreply.github.com>
2020-08-15 12:53:23 -0400

    About command now has the proper prefix.

--- bot/commands/about.js
+++ bot/commands/about.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const settings = require('../helpers/settings-manager');
+
 // Exports
 module.exports = {handle, getHelp};
 
@@ -15,7 +17,7 @@ function handle(client, msg) {
   embed.setColor('#FF0000');
   embed.setAuthor(client.user.username, client.user.displayAvatarURL());
 
-  embed.addField('__About__', 'The UC Esports bot is written in JS using the Discord.js library.\nSuggestions for the bot can be provided via the "/feedback" commmand.');
+  embed.addField('__About__', `The UC Esports bot is written in JS using the Discord.js library.\nSuggestions for the bot can be provided via the "${settings.getSettings().prefix}feedback" commmand.`);
   embed.addField('__Current Maintainers__', '• [Brendan Root](https://github.com/12brendan3)\n• [Baru Yogesh](https://github.com/BaruYogesh)');
   embed.addField('__Previous Contributors__', '• [Jordan Wentland](https://github.com/jordanwentland)');
   embed.addField('__Links__', '• [GitHub Repo](https://github.com/12brendan3/UC-Esports-Bot)');

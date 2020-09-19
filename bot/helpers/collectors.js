ed8f499f1addf28167c6482f3f98c6570b149fab
Brendan Root <12brendan3@users.noreply.github.com>
2020-09-19 16:53:28 -0400

    Added emoji collector

--- bot/helpers/collectors.js
+++ bot/helpers/collectors.js
@@ -1,9 +1,15 @@
 // Exports
-module.exports = {oneMessageFromUser};
+module.exports = {oneMessageFromUser, oneReactionFromUser};
 
 // Exported Functions
 async function oneMessageFromUser(channel, authorID) {
   const filter = (message) => message.author.id === authorID;
   const collected = await channel.awaitMessages(filter, {max: 1, time: 60000, errors: [`time`]});
+  return collected;
+}
+
+async function oneReactionFromUser(msg) {
+  const filter = (reaction, user) => user.id === msg.author.id;
+  const collected = await msg.awaitReactions(filter, {max: 1, time: 60000, errors: [`time`]});
   return collected;
 }

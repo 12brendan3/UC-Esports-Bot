018806315c26ad066a94b950bf68f0e1f5716f5a
Baru Yogesh <bhargav.yogesh@gmail.com>
2020-09-26 16:02:11 -0400

    functionality to update a guild's role reactions

--- bot/helpers/role-react-manager.js
+++ bot/helpers/role-react-manager.js
@@ -10,12 +10,22 @@
 // steps 1-3 are complete, rest remains.
 
 const database = require(`./database-manager`);
+const {Op} = require(`sequelize`);
 
 const categoryIDMap = new Map();
+let roleData = {};
 
 // load and return all categories
 async function loadCategories() {
   const categories = await database.getAllEntries(`RoleCategories`);
+  if (categories) {
+    return categories;
+  }
+  return false;
+}
+
+async function loadCategoriesOfGuild(guildId) {
+  const categories = await database.getAllEntries(`RoleCategories`, {guildID: guildId});
   if (categories) {
     return categories;
   }
@@ -31,6 +41,18 @@ async function loadRoles() {
   return false;
 }
 
+async function loadRolesOfCategories(categories) {
+  const categoryIds = [];
+
+  for (const cat of categories) {
+    categoryIds.push(cat.roleCategory);
+  }
+  const roles = await database.getAllEntries(`Roles`, {[Op.or]: categoryIds});
+  if (roles) {
+    return roles;
+  }
+  return false;
+}
 
 async function initRoles() {
   // load data
@@ -64,8 +86,75 @@ async function initRoles() {
     const cat = categoryIDMap.get(role.roleCategory);
     data[cat.guildID][cat.categoryName].roles.push(role);
   }
-
+  roleData = data;
   return data;
 }
 
-module.exports = {initRoles};
+async function updateGuildRoles(guildId) {
+  const categories = await loadCategoriesOfGuild(guildId);
+  const roles = await loadRolesOfCategories(guildId);
+
+  // if either database query did not work, return.
+  if (!(categories && roles)) {
+    return false;
+  }
+
+  roleData[guildId] = {};
+
+  // for each unique guild, for each unique category, an empty object to store roles.
+  for (const cat of categories) {
+    roleData[guildId][cat.categoryName] = {categoryDescription: cat.categoryDescription, roles: []};
+  }
+
+  // for each role, place the role data in its proper guild and category
+  for (const role of roles) {
+    // use the map to quickly identify which guild and category a role belongs to.
+    const cat = categoryIDMap.get(role.roleCategory);
+    roleData[guildId][cat.categoryName].roles.push(role);
+  }
+
+  return roleData;
+}
+
+/*
+data = {
+  guild1: {
+    guild1Category1: {
+      roles: [
+      role1,
+      role2,
+      role3
+      ]
+      categoryDescription: "..."
+    },
+    guild1Category2: {
+      roles: [
+      role1,
+      role2,
+      role3
+      ]
+      categoryDescription: "..."
+    },
+  },
+  guild1: {
+    guild1Category1: {
+      roles: [
+      role1,
+      role2,
+      role3
+      ]
+      categoryDescription: "..."
+    },
+    guild1Category2: {
+      roles: [
+      role1,
+      role2,
+      role3
+      ]
+      categoryDescription: "..."
+    },
+  }
+}
+*/
+
+module.exports = {initRoles, updateGuildRoles};

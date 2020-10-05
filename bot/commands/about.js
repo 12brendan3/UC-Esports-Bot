4e6f57341fa1194d3b38713577703089afb1b547
Brendan Root <12brendan3@users.noreply.github.com>
2020-10-05 02:54:18 -0400

    Improved about command

--- bot/commands/about.js
+++ bot/commands/about.js
@@ -1,6 +1,7 @@
 const Discord = require(`discord.js`);
 
 const settings = require(`../helpers/settings-manager`);
+const formatters = require(`../helpers/formatters`);
 
 // Exports
 module.exports = {handle, getHelp};
@@ -19,7 +20,7 @@ function handle(client, msg) {
   embed.setColor(`#FF7700`);
   embed.setAuthor(client.user.username, client.user.displayAvatarURL());
 
-  embed.addField(`__About__`, `The UC Esports bot is written in JS using the Discord.js library.\nSuggestions for the bot can be provided via the "${settings.getSettings().prefix}feedback" commmand.`);
+  embed.addField(`__About__`, `The UC Esports bot is written in JS using the Discord.js library.\nSuggestions for the bot can be provided via the "${settings.getSettings().prefix}feedback" commmand.\nThe bot is currently in ${client.guilds.cache.size} servers watching over ${formatters.formatNumber(client.users.cache.size)} users.`);
   embed.addField(`__Current Maintainers__`, `• [Brendan Root](https://github.com/12brendan3)\n• [Baru Yogesh](https://github.com/BaruYogesh)`);
   embed.addField(`__Previous Contributors__`, `• [Jordan Wentland](https://github.com/jordanwentland)`);
   embed.addField(`__Links__`, `• [GitHub Repo (Currently Private)](https://github.com/12brendan3/UC-Esports-Bot)`);

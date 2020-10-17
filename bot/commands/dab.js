8b84bf1ca0b10cb45d600b061f793e497c5365ad
Brendan Root <12brendan3@users.noreply.github.com>
2020-10-17 01:58:19 -0400

    "secret" dab command

---
+++ bot/commands/dab.js
@@ -0,0 +1,20 @@
+// Exports
+module.exports = {handle, getHelp};
+
+// Help command text
+const help = null;
+
+// Exported functions
+function handle(client, msg) {
+  const dabEmoji = client.emojis.cache.find((emoji) => emoji.name === `ReimuDab`);
+
+  if (dabEmoji && !dabEmoji.deleted) {
+    msg.channel.send(`${dabEmoji}`);
+  } else {
+    msg.reply(`there was an error running the command.`);
+  }
+}
+
+function getHelp() {
+  return help;
+}

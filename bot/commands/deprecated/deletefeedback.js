eb76ed2dc96d72953177a06fa4c6ae6f22d473d2
Brendan Root <12brendan3@users.noreply.github.com>
2022-05-05 23:15:43 -0400

    Make developer commands deprecated

---
+++ bot/commands/deprecated/deletefeedback.js
@@ -0,0 +1,42 @@
+const database = require(`../../helpers/database-manager`);
+const replyHelper = require(`../../helpers/interaction-helper`);
+
+// Exports
+module.exports = {handle, getHelp};
+
+// Help command text
+const help = {
+  text: `Allows developers to delete feedback from the database.`,
+  level: `developer`,
+  options: [
+    {
+      name: `entryid`,
+      type: `STRING`,
+      description: `The ID of the entry to remove.`,
+      required: true,
+    },
+  ],
+};
+
+// Exported functions
+function handle(client, interaction) {
+  removeFeedback(interaction);
+}
+
+async function removeFeedback(interaction) {
+  try {
+    const result = await database.removeEntry(`Feedback`, {ID: interaction.options.get(`entryid`).value});
+    if (result) {
+      replyHelper.interactionReply(interaction, {content: `The feedback entry was removed.`, ephemeral: true});
+    } else {
+      replyHelper.interactionReply(interaction, {content: `No feedback with that ID was found.`, ephemeral: true});
+    }
+  } catch (err) {
+    console.error(err);
+    replyHelper.interactionReply(interaction, {content: `There was an error.`, ephemeral: true});
+  }
+}
+
+function getHelp() {
+  return help;
+}
---
+++ bot/commands/deprecated/review.js
@@ -0,0 +1,76 @@
+const Discord = require(`discord.js`);
+
+const database = require(`../../helpers/database-manager`);
+const replyHelper = require(`../../helpers/interaction-helper`);
+
+// Exports
+module.exports = {handle, getHelp};
+
+// Help command text
+const help = {
+  text: `Allows developers to review bot feedback.`,
+  level: `developer`,
+};
+
+// Exported functions
+function handle(client, interaction) {
+  sendFeedback(client, interaction);
+}
+
+async function sendFeedback(client, interaction) {
+  try {
+    const result = await database.getAllEntries(`Feedback`);
+
+    if (result && result.length < 1) {
+      replyHelper.interactionReply(interaction, `There is no feedback.`);
+      return;
+    }
+
+    const embeds = [];
+
+    for (let i = 0; i < result.length; i++) {
+      const embed = new Discord.MessageEmbed();
+
+      let user = client.users.cache.get(result[i].userID);
+      let displayURL = client.user.displayAvatarURL();
+
+      if (user) {
+        displayURL = user.displayAvatarURL();
+        user = user.tag;
+      } else {
+        user = result[i].userID;
+      }
+
+      let link = result[i].messageURL;
+      if (result[i].messageURL.startsWith(`https://`)) {
+        link = `[View Message](${result[i].messageURL})`;
+      } else if (result[i].messageURL !== `Sent via DM`) {
+        link = `<#${result[i].messageURL}>`;
+      }
+
+      embed.setColor(`#CC00FF`);
+      embed.setAuthor(user, displayURL);
+      embed.setTimestamp(result[i].time);
+
+      embed.addField(`Entry ID`, result[i].ID);
+      if (result[i].message) {
+        embed.addField(`Feedback`, result[i].message.length > 1000 ? result[i].message.substr(0, 1000) : result[i].message);
+        if (result[i].message.length > 1000) {
+          embed.addField(`Feedback Continued`, result[i].message.substr(1000, result[i].message.length));
+        }
+      }
+      embed.addField(`Message Link`, link);
+
+      embeds.push(embed);
+    }
+
+    replyHelper.interactionReply(interaction, {embeds});
+  } catch (err) {
+    console.error(err);
+    replyHelper.interactionReply(interaction, `There was an error.`);
+  }
+}
+
+function getHelp() {
+  return help;
+}
